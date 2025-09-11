c9b24a878eb717ebd48878bf4822716b7261f8e6
Andualem Zeleke <andzed916@gmail.com>
2025-09-11 15:29:36 +0300

    first commit

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,17 @@
+import AppHeader from "../components/app-header";
+import BackgroundPattern from "../components/background-pattern";
+
+export default function Layout({children}:{
+    children:React.ReactNode;
+}) {
+  return ( 
+    <>
+    <BackgroundPattern />
+    <div className="flex flex-col gap-y-10 max-w-[1050px] mx-auto min-h-screen">
+    <AppHeader />
+    {children}
+    </div>
+    
+    </>
+  )
+}
---
+++ src/app/components/expenses-list.tsx
@@ -0,0 +1,7 @@
+export default function ExpensesList() {
+  return (
+    <main>
+        <ul className="h-[300px] rounded mt-4 shadow-md bg-white"></ul>
+    </main>
+  )
+}
